d598444662d1aef72ab3ce84a1bff1b1c9211b64
tranj5371 <47762230+tranj5371@users.noreply.github.com>
2019-02-25 12:47:52 -0500

    For Loop for Business days and Sales

---
+++ Business Days and Commission.py
@@ -0,0 +1,7 @@
+#ask user how many days business is open, enter amount of sales each day
+
+DaysOpen = int(input("Enter how many days are you open? "))
+
+for DaysOpen in range (1, DaysOpen+1):
+    Sales = int(input("Enter amount of sales: "))
+
