07deefe3888f8db39d0e98e0db089ca85fd2e0d9
tranj5371 <47762230+tranj5371@users.noreply.github.com>
2019-02-25 12:23:57 -0500

    Calculating Dog Years via For Loop

---
+++ Dog Years.py
@@ -0,0 +1,9 @@
+for Age in [10, 20, 30]:
+    print ("When you are", Age, ",you are", Age * 7, "in dog years")
+    
+#or
+print("Age\tDog Years")
+print("-----------------")
+for age in [10, 20, 30]:
+    dogyears = age * 7
+    print(age, "\t",dogyears)
