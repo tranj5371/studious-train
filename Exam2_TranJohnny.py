3efe3e440b66969431d70ab35aac11211d57268c
tranj5371 <47762230+tranj5371@users.noreply.github.com>
2019-02-27 12:06:55 -0500

    For Loop for Time and Distance

---
+++ Exam2_TranJohnny.py
@@ -0,0 +1,11 @@
+#Puts a table
+print("Time (Hours)\tDistance (Miles)")
+print("-------------------------------")
+
+#Calculates and Displays distance traveled in 3, 7, and 15 hours when speed is at 70 mph
+for num in [3, 7, 15]:
+    Distance = num * 70
+    print(num, "       \t", Distance)
+    
+#Displays the speed
+print("The speed is 70 miles per hour.")
